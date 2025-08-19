3a88423a196102eb6623238849c125079a56d8c2
Sahil Pandey <85783436+pandey-sahil@users.noreply.github.com>
2025-08-19 10:13:58 +0530

    Update list-hover-anime.js

--- list-hover-anime.js
+++ list-hover-anime.js
@@ -1,28 +1,37 @@
-
+<script type="module">
 import * as THREE from "three";
 import { EffectComposer } from "three/addons/postprocessing/EffectComposer.js";
 import { RenderPass } from "three/addons/postprocessing/RenderPass.js";
 import { ShaderPass } from "three/addons/postprocessing/ShaderPass.js";
 import { RGBShiftShader } from "three/addons/shaders/RGBShiftShader.js";
 
+// Each item gets its own effect instance
 class RGBShiftEffect {
-  constructor(container, itemsWrapper, { strength = 0.002 } = {}) {
-    this.container = container;
-    this.itemsWrapper = itemsWrapper;
+  constructor(wrapper, img, { strength = 0.003 } = {}) {
+    this.wrapper = wrapper;
+    this.img = img;
+
+    // create canvas inside wrapper
+    this.canvas = document.createElement("canvas");
+    this.canvas.classList.add("portfolio20-canvas");
+    this.wrapper.appendChild(this.canvas);
+
+    // hide the native img
+    this.img.style.display = "none";
 
     // setup renderer, scene, camera
-    this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
-    this.renderer.setSize(window.innerWidth, window.innerHeight);
-    this.container.appendChild(this.renderer.domElement);
+    this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true, canvas: this.canvas });
+    this.renderer.setSize(this.wrapper.clientWidth, this.wrapper.clientHeight);
+    this.renderer.setPixelRatio(window.devicePixelRatio);
 
     this.scene = new THREE.Scene();
-    this.camera = new THREE.PerspectiveCamera(
-      45,
-      window.innerWidth / window.innerHeight,
-      0.1,
-      100
-    );
-    this.camera.position.z = 2;
+    this.camera = new THREE.OrthographicCamera(-1, 1, 1, -1, 0, 1);
+
+    // load texture from <img>
+    const tex = new THREE.TextureLoader().load(this.img.src);
+    const geometry = new THREE.PlaneGeometry(2, 2);
+    const material = new THREE.MeshBasicMaterial({ map: tex });
+    this.scene.add(new THREE.Mesh(geometry, material));
 
     // composer
     this.composer = new EffectComposer(this.renderer);
@@ -32,6 +41,7 @@ class RGBShiftEffect {
     rgbPass.uniforms["amount"].value = strength;
     this.composer.addPass(rgbPass);
 
+    // start loop
     this.animate();
   }
 
@@ -41,6 +51,13 @@ class RGBShiftEffect {
   }
 }
 
-// Example usage
-const wrapper = document.querySelector(".portfolio20_list");
-new RGBShiftEffect(wrapper, wrapper, { strength: 0.003 });
+// Init for all items
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll(".portfolio20_item-link").forEach(item => {
+    const img = item.querySelector(".portfolio20_image");
+    if (img) {
+      new RGBShiftEffect(item, img, { strength: 0.003 });
+    }
+  });
+});
+</script>
