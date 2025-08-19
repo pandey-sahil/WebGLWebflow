2d2a2501ee2831e6a249eb757fdd32f9e1608473
Sahil Pandey <85783436+pandey-sahil@users.noreply.github.com>
2025-08-19 10:12:10 +0530

    Update list-hover-anime.js

--- list-hover-anime.js
+++ list-hover-anime.js
@@ -1,89 +1,46 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // Add canvas to each image wrapper
-  document.querySelectorAll(".portfolio20_image-wrapper").forEach(wrapper => {
-    const canvas = document.createElement("canvas");
-    canvas.classList.add("portfolio20-canvas");
-    wrapper.appendChild(canvas);
-    // Hide the native image
-    const img = wrapper.querySelector(".portfolio20_image");
-    if (img) img.style.display = "none";
-  });
 
-  // extend EffectShell get items
-  class EffectShell {
-    constructor(container = document.body, itemsWrapper = null) {
-      this.container = container
-      this.itemsWrapper = itemsWrapper
-      if (!this.container || !this.itemsWrapper) return
-      this.setup()
-      this.initEffectShell().then(() => {
-        this.isLoaded = true
-        if (this.isMouseOver) this.onMouseOver(this.tempItemIndex)
-        this.tempItemIndex = null
-      })
-      this.createEventsListeners()
-    }
+import * as THREE from "three";
+import { EffectComposer } from "three/addons/postprocessing/EffectComposer.js";
+import { RenderPass } from "three/addons/postprocessing/RenderPass.js";
+import { ShaderPass } from "three/addons/postprocessing/ShaderPass.js";
+import { RGBShiftShader } from "three/addons/shaders/RGBShiftShader.js";
 
-    setup() {
-      window.addEventListener('resize', this.onWindowResize.bind(this), false)
-      this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true, canvas: this.getCanvas() })
-      this.renderer.setSize(this.viewport.width, this.viewport.height)
-      this.renderer.setPixelRatio(window.devicePixelRatio)
+class RGBShiftEffect {
+  constructor(container, itemsWrapper, { strength = 0.002 } = {}) {
+    this.container = container;
+    this.itemsWrapper = itemsWrapper;
 
-      this.scene = new THREE.Scene()
-      this.camera = new THREE.PerspectiveCamera(40, this.viewport.aspectRatio, 0.1, 100)
-      this.camera.position.set(0, 0, 3)
-      this.mouse = new THREE.Vector2()
-      this.timeSpeed = 2
-      this.time = 0
-      this.clock = new THREE.Clock()
-      this.renderer.setAnimationLoop(this.render.bind(this))
-    }
+    // setup renderer, scene, camera
+    this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    this.container.appendChild(this.renderer.domElement);
 
-    getCanvas() {
-      // get the first canvas we injected
-      return this.itemsWrapper.querySelector(".portfolio20-canvas")
-    }
+    this.scene = new THREE.Scene();
+    this.camera = new THREE.PerspectiveCamera(
+      45,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      100
+    );
+    this.camera.position.z = 2;
 
-    render() {
-      this.time += this.clock.getDelta() * this.timeSpeed
-      this.renderer.render(this.scene, this.camera)
-    }
+    // composer
+    this.composer = new EffectComposer(this.renderer);
+    this.composer.addPass(new RenderPass(this.scene, this.camera));
 
-    initEffectShell() {
-      let promises = []
-      this.items = this.itemsElements
-      const loader = new THREE.TextureLoader()
-      this.items.forEach((item, index) => {
-        const url = item.img ? item.img.src : null
-        promises.push(this.loadTexture(loader, url, index))
-      })
-      return Promise.all(promises).then(promises => {
-        promises.forEach((p, i) => {
-          this.items[i].texture = p.texture
-        })
-      })
-    }
+    const rgbPass = new ShaderPass(RGBShiftShader);
+    rgbPass.uniforms["amount"].value = strength;
+    this.composer.addPass(rgbPass);
 
-    get itemsElements() {
-      const items = [...this.itemsWrapper.querySelectorAll('.portfolio20_item-link')]
-      return items.map((item, index) => ({
-        element: item,
-        img: item.querySelector('.portfolio20_image') || null,
-        index
-      }))
-    }
-
-    loadTexture(loader, url, index) {
-      return new Promise((resolve, reject) => {
-        if (!url) { resolve({ texture: null, index }); return }
-        loader.load(url, tex => resolve({ texture: tex, index }), undefined, err => reject(err))
-      })
-    }
+    this.animate();
   }
 
-  // RGBShiftEffect stays same...
+  animate() {
+    requestAnimationFrame(() => this.animate());
+    this.composer.render();
+  }
+}
 
-  const wrapper = document.querySelector('.portfolio20_list')
-  new RGBShiftEffect(wrapper, wrapper, { strength: 0.3 })
-})
+// Example usage
+const wrapper = document.querySelector(".portfolio20_list");
+new RGBShiftEffect(wrapper, wrapper, { strength: 0.003 });
