02864589e2fb6aaab4b2d0d0ee7f4110e79516f3
Sahil Pandey <85783436+pandey-sahil@users.noreply.github.com>
2025-08-26 00:37:05 +0530

    Update neokred.js

--- neokred.js
+++ neokred.js
@@ -1,5 +1,5 @@
 import * as THREE from "three"; 
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js"; // ✅ use examples/jsm
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 
 // Scene
 const scene = new THREE.Scene();
