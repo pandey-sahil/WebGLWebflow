6894ee992a820e366390067d18af3ca4494356f8
Nikhil Kushwaha <169012845+nikhil257@users.noreply.github.com>
2025-08-26 09:56:18 +0530

    camera per

--- neokred.js
+++ neokred.js
@@ -48,7 +48,7 @@ loader.load(
     model.position.sub(center);
 
     // ✅ Reset rotation so it's perfectly front-facing
-    model.rotation.set(0, 0, 0);
+    model.rotation.set(2, 2, 0);
 
     // Material cleanup
     model.traverse((child) => {
